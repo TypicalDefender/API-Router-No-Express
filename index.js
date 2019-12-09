2ca1cbf9dffe0a80e42f67a0e77a2079efe71697
Sarthak Dalabehera <37482550+TypicalDefender@users.noreply.github.com>
2019-12-09 19:51:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,110 +1,80 @@
-//@sarthak_acoustic
-//openssl req -newkey rsa:2048 -new -nodes -x509 -days 3650 -keyout key.pem -out cert.pem
-const http = require('http');
-const https = require('https');
-const url = require('url');
-const StringDecoder = require('string_decoder').StringDecoder;
-const config = require('./config');
-const fs = require('fs');
-const _data = require('./lib/data');
+import { getModel as MasterResultModel } from '../masterResults/masterResults.model';
+import { getModel as StudentModel } from '../../settings/student/student.model';
 
-//@TODO -> TESTING
-_data.update('test','newFile',{'name' : 'dev'}, (err)=>{
-  console.log('the error is', err);
-})
-
-// _data.read('test','newFile', (err, data)=>{
-//   console.log('the error is', err, ' and the data is',data);
-// })
-
-const httpServer = http.createServer((req, res)=>{
-    unifiedServer(req, res);
-});
-
-  // //GET the headers as an Object
-  // //console.log('the requested path is : '+trimmedpath + ' with method : '+method+' with query string object ',queryStringObject);
-  // console.log('the requested headers are : ', headers);
-
-httpServer.listen(config.httpPort, ()=>{
-  console.log('the server is listening on port '+config.httpPort+' and in '+config.envName+' mode');
-});
-
-// var httpsServerOptions = {
-//   'key' : fs.readFileSync('./https/key.pem'),
-//   'cert' : fs.readFileSync('./https/cert.pem')
-// };
-//creating a httpsServer with httpsServerOptions(for encryption)
-// const httpsServer = https.createServer(httpsServerOptions, (req, res)=>{
-//   unifiedServer(req, res);
-// });
-//
-// httpsServer.listen(config.httpsPort, ()=>{
-//   console.log('the server is listening on port '+config.httpsPort);
-// });
-
-const unifiedServer = (req, res)=>{
-  //##parsing the requested url path
-  const pathURL = url.parse(req.url, true);
-  const path = pathURL.pathname;
-  //regex
-  const trimmedpath = path.replace(/^\/+|\/+$/g, '');
-  //##parsing query string object
-  const queryStringObject = pathURL.query;
-
-  //##parsing http request method
-  const method = req.method.toUpperCase();
-  //headers
-  var headers = req.headers;
-  //GET the payload, if any
-  var decoder = new StringDecoder('utf-8');
-  var buffer = ''
-  req.on('data', (data)=>{
-    buffer += decoder.write(data);
-  });
-  req.on('end', ()=>{
-    buffer += decoder.end();
-
-
-    var choosenHandler = typeof(router[trimmedpath]) !== 'undefined' ? router[trimmedpath] : handlers.notFound;
-    //construct the data object that to send to the handler
-    var data = {
-      'trimmedpath' : trimmedpath,
-      'queryStringObject': queryStringObject,
-      'method': method,
-      'payload': buffer,
-      'headers': headers
+export async function getMasterResults(args, context) {
+  args = args.input; //eslint-disable-line
+  if (!args.studentId) throw new Error('StudentId is required');
+  // console.log('args', args);
+  const query = { studentId: args.studentId };
+  if (args.questionPaperIds) {
+    query.questionPaperId = {
+      $in: args.questionPaperIds,
     };
+  }
+  const resultsObj = {};
+  return MasterResultModel(context).then(MasterResult => MasterResult.find(query, {
+    studentId: 1, obtainedMarks: 1, questionPaperId: 1, cwuAnalysis: 1, responseData: 1,
+  }).then((objs) => {
+    if (objs && objs.length) {
+      resultsObj.studentId = objs[0].studentId;
+      const results = [];
+      objs.forEach((obj) => {
+        results.push({
+          obtainedMarks: obj.obtainedMarks,
+          questionPaperId: obj.questionPaperId,
+          C: obj.cwuAnalysis.C,
+          W: obj.cwuAnalysis.W,
+          U: obj.cwuAnalysis.U,
+          responseData: obj.responseData,
+        });
+      });
+      resultsObj.results = results;
+      return resultsObj;
+    }
+    return {};
+  }));
+}
 
-  choosenHandler(data, (statusCode, payload)=>{
-    statusCode = typeof(statusCode) == 'number' ? statusCode : 200;
+export async function getStudentList(req, res){
+  if(!req.params || !req.params.paperId){
+      throw new Error('Please provide questionPaperId');
+  }
+  const branchName = req.user_cxt.rawHierarchy[4].child;
+  if(!branchName){
+      throw new Error('branchName is not available');
+  }
+  const Student = await StudentModel(req.user_cxt);
+  const masterresults = await MasterresultModel(req.user_cxt);
+  const studentInfo = await Student.aggregate([{
+  $match : {"hierarchy.child" : branchName}},
+   {$group : {_id : {studentId : "$studentId", 
+   studentName : "$studentName"}}}]);
+  const studentIdArray = studentInfo.map(x => x._id.studentId);
+  if(!studentIdArray.length){
+      console.error("No practice data is available");
+  }
+  const studentNameArray = studentInfo.map(x => x._id.studentName);
+  if(studentNameArray.length !== studentIdArray.length){
+      throw new Error('Data is missing');
+  }
+  const masterresultData = await masterresults
+   .aggregate([{$match : {questionPaperId : req.params.paperId,
+   studentId : {$in : studentIdArray}}} 
+   ,{$sort : {"updated_at" : -1}},  
+   {"$group": {"_id": "$studentId", 
+   "cwuAnalysis" : {"$first" : "$cwuAnalysis"}, 
+   "obtainedMarks": {"$first" : "$obtainedMarks"}}}]);
+  //res.send(masterresultData);
+  for(let i = 0; i < masterresultData.length; i += 1){
+      let index = 0;
+      if(studentIdArray.includes(masterresultData[i]._id)){
+          index = studentIdArray.indexOf(masterresultData[i]._id);
+          masterresultData[i].studentName = studentNameArray[index];
+      }
+  }
+  res.send(masterresultData);
+}
 
-    payload = typeof(payload) == 'object' ? payload : {};
 
-    var payloadString = JSON.stringify(payload);
-    //returing JSON headers
-    res.setHeader('Content-Type', 'application/json');
-    res.writeHead(statusCode);
-    res.end(payloadString);
-    console.log('returning the response : ', statusCode, payloadString);
-     })
-  })
-};
 
-var handlers = {};
-//sample handler
-// handlers.sample = (data, callback)=>{
-//   callback(406, {"name" : "sample handler"});
-// };
-
-//ping handler [used to check whether our app is alive or not]
-handlers.ping = (data, callback)=>{
-  callback(200);
-};
-//notFound handler
-handlers.notFound = (data, callback)=>{
-  callback(404);
-};
-//define a request router
-var router = {
-  'ping' : handlers.ping
-};
+export default { getMasterResults, getStudentList };
