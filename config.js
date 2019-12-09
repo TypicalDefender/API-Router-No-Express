e739d9ef48e83d68ead5a9356370833a9ba8bea2
Sarthak Dalabehera <37482550+TypicalDefender@users.noreply.github.com>
2019-12-09 19:52:19 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,28 +1,63 @@
-/*
-*
-*  create and export configuration variables
-*
-*/
-var environments = {};
+const express = require('express');
+const router = express.Router();
 
-//Staging (default) environment
-environments.staging = {
-  'httpPort' : 3000,
-  'httpsPort': 3001,
-  'envName': 'staging'
-};
+const controller = require('./practiceStudentAnalysis.controller');
 
-//production environment
-environments.production = {
-  'httpPort' : 5000,
-  'httpsPort': 5001,
-  'envName': 'production'
-};
-//Determine which argument is passed as a command-line-argument
-var currentEnvironment = typeof(process.env.NODE_ENV) == 'string' ? process.env.NODE_ENV.toLowerCase() : '';
+router.get('/studentList/:paperId', controller.getStudentList);
 
-//check the current environment is one of the above, if not default to staging!!
-var environmentToExport = typeof(environments[currentEnvironment]) == 'object' ? environments[currentEnvironment] : environments.staging;
+module.exports = router;
 
-//export the module
-module.exports = environmentToExport;
+
+
+
+
+
+
+
+/**
+ * API
+ */
+
+const concpetTaxonomy = require('./settings/conceptTaxonomy');
+const instituteHierarchy = require('./settings/instituteHierarchy');
+const contentMapping = require('./settings/contentMapping');
+const textbook = require('./settings/textbook');
+const testUpload = require('./tests/questions');
+const studentLedger = require('./studentLedger');
+const practiceStudent = require('./tests/masterResults');
+// Util modules
+const uploadFile = require('./v1/uploadFile');
+const downloadReports = require('./v1/downloadReports');
+const user = require('./v1/user');
+const auth = require('../auth').default;
+const authValidation = require('./v1/authValidation');
+const converter = require('./v1/converter');
+const studentSync = require('./v1/studentSync');
+const hierarchySync = require('./v1/hierarchySync');
+const elasticindexing = require('./indexelasticsearch/indexcontent/index');
+const test = require('./tests/upload');
+const timeAnalysis = require('./analysis/timeAnalysis');
+
+export default function (app) {
+  //  Insert API below
+  app.use('/api/concpetTaxonomy', concpetTaxonomy);
+  app.use('/api/instituteHierarchy', instituteHierarchy);
+  app.use('/api/contentMapping', contentMapping);
+  app.use('/api/textbook', textbook);
+  app.use('/api/practice',testUpload);
+  app.use('/api/test',test);
+  app.use('/api/studentLedger', studentLedger);
+
+  // Util APIs
+  app.use('/api/v1/converter', converter);
+  app.use('/api/v1/uploadFile', uploadFile);
+  app.use('/api/v1/users', user);
+  app.use('/api/v1/downloadReports', downloadReports);
+  app.use('/auth', auth);
+  app.use('/api/v1/authValidation', authValidation);
+  app.use('/api/v1/studentSync', studentSync);
+  app.use('/api/v1/hierarchySync', hierarchySync);
+  app.use('/api/elastic', elasticindexing);
+  app.use('/api/timeAnalysis', timeAnalysis);
+  app.use('/api/practiceStudent', practiceStudent);
+}
